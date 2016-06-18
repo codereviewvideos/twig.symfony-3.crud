5a8142d596be76b5c148c260628cc5507e1167da
Chris <chris@codereviewvideos.com>
2016-06-18 10:57:24 +0100

    changes

--- src/AppBundle/Entity/BlogPost.php
+++ src/AppBundle/Entity/BlogPost.php
@@ -20,15 +20,33 @@ class BlogPost implements \JsonSerializable
     /**
      * @ORM\Column(type="string", name="title")
      */
-    private $title;
+    protected $title;
 
     /**
      * @ORM\Column(type="string", name="body")
      */
-    private $body;
+    protected $body;
+
+    /**
+     * @ORM\Column(name="decimal_1", type="decimal", scale=2, precision=8)
+     */
+    protected $decimal1;
+
+    /**
+     * @ORM\Column(name="decimal_2", type="decimal", scale=3, precision=8)
+     */
+    protected $decimal2;
+
+    /**
+     * @ORM\Column(name="decimal_3", type="decimal",  precision=10, scale=4)
+     */
+    protected $decimal3;
+
 
     public function getId()
     {
+        // this has the word private in it
+        // and a semi colon ; for some reason
         return $this->id;
     }
 
